3b8a04bc038d538e09545d107f79295863702048
Utada-san <134759891+RamaDutasmara@users.noreply.github.com>
2025-11-06 21:47:32 +0700

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,7 @@
+Cypress.Commands.add('loginUi', (username, password) => {
+  cy.visit('/');
+  cy.get('input[name="username"]').clear().type(username);
+  cy.get('input[name="password"]').clear().type(password);
+  cy.get('button[type="submit"]').click();
+});
 
