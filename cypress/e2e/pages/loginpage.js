0f7495de48197f44fbecc483afe2e2d364e8495e
Utada-san <134759891+RamaDutasmara@users.noreply.github.com>
2025-11-06 21:46:07 +0700

    Update loginpage.js

--- cypress/e2e/pages/loginpage.js
+++ cypress/e2e/pages/loginpage.js
@@ -1,1 +1,30 @@
+class LoginPage {
+  visit() {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+  }
+
+  usernameInput() {
+    return cy.get('input[name="username"]');
+  }
+
+  passwordInput() {
+    return cy.get('input[name="password"]');
+  }
+
+  loginButton() {
+    return cy.get('button[type="submit"]');
+  }
+
+  errorMessage() {
+    return cy.get('.oxd-alert-content-text');
+  }
+
+  login(username, password) {
+    this.usernameInput().type(username);
+    this.passwordInput().type(password);
+    this.loginButton().click();
+  }
+}
+
+export default new LoginPage();
 
