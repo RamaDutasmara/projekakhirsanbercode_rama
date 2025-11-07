f85f0b0d853766c6ffe06b57a68bd8da10d804ab
Utada-san <134759891+RamaDutasmara@users.noreply.github.com>
2025-11-07 21:14:07 +0700

    Update loginpage.js

--- cypress/e2e/pages/loginpage.js
+++ cypress/e2e/pages/loginpage.js
@@ -1,30 +1,38 @@
+// cypress/pages/LoginPage.js
 class LoginPage {
+  elements = {
+    usernameInput: () => cy.get('input[name="username"]'),
+    passwordInput: () => cy.get('input[name="password"]'),
+    loginButton: () => cy.get('button[type="submit"]'),
+    forgotPasswordLink: () => cy.get('.orangehrm-login-forgot-header'),
+    alertMessage: () => cy.get('.oxd-alert-content-text'),
+    logoutMenu: () => cy.get('.oxd-userdropdown-tab'),
+    dashboardHeader: () => cy.get('.oxd-topbar-header-breadcrumb > h6')
+  };
+
   visit() {
-    cy.visit('https://opensource-demo.orangehrmlive.com/');
-  }
-
-  usernameInput() {
-    return cy.get('input[name="username"]');
-  }
-
-  passwordInput() {
-    return cy.get('input[name="password"]');
-  }
-
-  loginButton() {
-    return cy.get('button[type="submit"]');
-  }
-
-  errorMessage() {
-    return cy.get('.oxd-alert-content-text');
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   }
 
   login(username, password) {
-    this.usernameInput().type(username);
-    this.passwordInput().type(password);
-    this.loginButton().click();
+    if (username) this.elements.usernameInput().type(username);
+    if (password) this.elements.passwordInput().type(password);
+    this.elements.loginButton().click();
+  }
+
+  logout() {
+    this.elements.logoutMenu().click({ force: true });
+    cy.contains('Logout').click({ force: true });
+  }
+
+  validateDashboard() {
+    cy.url().should('include', '/dashboard');
+    this.elements.dashboardHeader().should('contain', 'Dashboard');
+  }
+
+  validateLoginError(message) {
+    this.elements.alertMessage().should('contain', message);
   }
 }
 
 export default new LoginPage();
-
