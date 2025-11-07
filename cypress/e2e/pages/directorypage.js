ecb39bb3b0d19a310ac3df0bea644b5d3922ca95
Utada-san <134759891+RamaDutasmara@users.noreply.github.com>
2025-11-07 21:12:45 +0700

    Update directorypage.js

--- cypress/e2e/pages/directorypage.js
+++ cypress/e2e/pages/directorypage.js
@@ -1,1 +1,23 @@
+class DirectoryPage {
+  url = 'https://opensource-demo.orangehrmlive.com/web/index.php/directory/viewDirectory';
+
+  elements = {
+    directoryTab: () => cy.get('a[href*="directory/viewDirectory"]').first(),
+    nameInput: () => cy.get('.oxd-input').first(),
+    searchButton: () => cy.contains('Search'),
+    resetButton: () => cy.contains ('Reset'),
+    cardResult: () => cy.get('.oxd-grid-item'),
+    noResult: () => cy.contains('No Records Found'),
+    jobTitleDropdown: () => cy.get('.oxd-select-wrapper').eq(0),
+    locationDropdown: () => cy.get('.oxd-select-wrapper').eq(1),
+    tableCard: () => cy.get('.oxd-table-card'),
+
+  }
+
+  visit() {
+    cy.visit(this.url);
+  }
+}
+
+export default new DirectoryPage();
 
